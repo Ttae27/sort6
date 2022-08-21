fb2df16ad5d2518402322e7657c7898b58a49f68
Ttae27 <ponghong.2744@gmail.com>
2022-08-22 00:21:55 +0700

    readyToServe

--- sort6_1.c
+++ sort6_1.c
@@ -7,22 +7,22 @@ void    sort6(char **str)
     char    *tmp;
     int     k,j;
 
+    // Selection_Sort
     for (int i = 0;i < 6;i++)
     {
-        tmp = str[i];
-        j = i + 1;
-        while (j < 6)
+        int j_min = i;
+        for (j = i + 1;j < 6;j++)
+            if (strcmp(str[j], str[j_min]) < 0)
+                j_min = j;
+        if (j_min != i)
         {
-            if (strcmp(tmp, str[j]) > 0)
-            {
-                tmp = str[j];
-                str[j] = str[i];
-            }
-            j++;
+            strcpy(tmp, str[i]);
+            strcpy(str[i], str[j_min]);
+            strcpy(str[j_min], tmp);
         }
-        str[i] = tmp;
     }
     k = 0;
+    printf("-------------------------------\n");
     while (k < 6)
     {
         while(k < 5 && strcmp(str[k], str[k + 1]) == 0)
@@ -41,7 +41,7 @@ int main()
 
     str = (char **)malloc(sizeof(char *) * 6);
     for (int i = 0;i < 6;i++)
-        str[i] = (char *)malloc(sizeof(char) * 100);
+        str[i] = (char *)malloc(sizeof(char) * 30);
     for (int i = 0;i < 6;i++)
         scanf("%s", str[i]);
     sort6(str);
